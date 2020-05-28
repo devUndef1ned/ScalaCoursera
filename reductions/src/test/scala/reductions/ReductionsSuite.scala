da4072d8ebe398e70229c71a86e6496e4fb8af31
Oleg Kuzmin <o.kuzmin@dodopizza.com>
2020-05-28 21:56:09 +0300

    Added reduction parallel version

--- reductions/src/test/scala/reductions/ReductionsSuite.scala
+++ reductions/src/test/scala/reductions/ReductionsSuite.scala
@@ -18,7 +18,11 @@ class ReductionsSuite {
     assertEquals(List(0f, 1f, 4f, 4f), output.toList)
   }
 
-
+  @Test def `parLineOfSight should correctly handle an array of size 4`: Unit = {
+    val output = new Array[Float](4)
+    parLineOfSight(Array[Float](0f, 1f, 8f, 9f), output, 2)
+    assertEquals(List(0f, 1f, 4f, 4f), output.toList)
+  }
 
 
   /*******************************
